1b1d3cf10ac41c96281715948c6bfc0b050963db
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2021-11-27 10:50:11 +0100

    Add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Import components
+import { HomeComponent } from './modules/views/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import components
+import { HomeComponent } from './modules/views/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Components
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +20,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
