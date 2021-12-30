a06c63164149301bd0c19aedc15b70a179015f05
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2021-12-30 10:48:18 +0100

    Update contact component

--- src/app/modules/views/home/home-contact/home-contact.component.ts
+++ src/app/modules/views/home/home-contact/home-contact.component.ts
@@ -3,9 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-contact',
   templateUrl: './home-contact.component.html',
-  styleUrls: ['./home-contact.component.scss']
+  styleUrls: [
+    './home-contact.component.scss',
+    '../../../../../assets/styles/material.scss'
+  ]
 })
 export class HomeContactComponent implements OnInit {
+
+  contactText = 'Kontakt';
+  sendBtnText = 'Wyślij';
 
   constructor() { }
 
