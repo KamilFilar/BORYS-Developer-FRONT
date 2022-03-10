d8f5f13358975b490106771d3b945c38c395f7a6
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-03-10 17:30:38 +0100

    Corrects router in offer component

--- src/app/modules/components/offer/offer.component.ts
+++ src/app/modules/components/offer/offer.component.ts
@@ -4,10 +4,9 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
-  styleUrls: ['./offer.component.scss']
+  styleUrls: ['./offer.component.scss'],
 })
 export class OfferComponent implements OnInit {
-
   @Input() offerID = '';
   @Input() offerImagePath = '';
   @Input() offerTitle = '';
@@ -20,40 +19,31 @@ export class OfferComponent implements OnInit {
   @Input() offerPrice = '';
 
   constructor(
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
 
-  firstLetterUp(item: string){
+  firstLetterUp(item: string) {
     return item.charAt(0).toUpperCase() + item.slice(1);
   }
 
   goToDetails(category: any) {
-    if(category == 'lokal')
-      this.router.navigate(['Lokale/', this.offerID]);
-    if(category == 'dom')
-      this.router.navigate(['Domy/', this.offerID]);
-    if(category == 'mieszkanie')
-      this.router.navigate(['Mieszkania/', this.offerID]);
-    if(category == 'działka')
-      this.router.navigate(['Działki/', this.offerID]);
-    
+    if (category == 'Lokal') this.router.navigate(['../Lokale/', this.offerID]);
+    if (category == 'Dom') this.router.navigate(['Domy/', this.offerID]);
+    if (category == 'Mieszkanie') this.router.navigate(['Mieszkania/', this.offerID]);
+    if (category == 'Działka') this.router.navigate(['Działki/', this.offerID]);
     return;
   }
 
   calcPriceArea() {
     let price = this.offerPrice.replace(/\s/g, '');
-    return (parseFloat(price)/parseFloat(this.offerArea)).toFixed(2);
+    return (parseFloat(price) / parseFloat(this.offerArea)).toFixed(2);
   }
-  
-  isPlot() {
-    if(this.offerCategory == 'działka')
-      return true;
 
-    return false;
+  isPlot() {
+    return this.offerCategory == 'Działka' ? true : false;
   }
 
   ngOnInit(): void {
     this.offerCategory = this.firstLetterUp(this.offerCategory);
   }
-
 }
